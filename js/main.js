efb259435d7bf21bf2a1c1260d556d47231f60d4
Arturo <Arturojportillo@gmail.com>
2024-08-06 04:30:45 -0300

    JavaScript 2024 Comision #59955

---
+++ js/main.js
@@ -0,0 +1,82 @@
+alert("Bienvenido a MiBar! Sistema de gestion gastronomica"+ "\n\n" +"Para iniciar sesion por primera vez debes registrarte.")
+
+
+/* Variable usuario y contraseña */
+let usuario = "";
+let contrasena = "";
+let idEmpleado = 0;
+let rol = '';
+
+/* Funcion constructora de usuario estableciendo su rol */
+
+function Empleado(usuario, contrasena, idEmpleado, rol) {
+    this.usuario = usuario;
+    this.contrasena = contrasena;
+    this.idEmpleado = idEmpleado;
+    this.rol = rol;
+}
+
+
+/* Funcion de registro */
+
+let empleado;
+
+function signUp() {
+
+    let nuevoUsuario = prompt("Ingresa nuevo nombre de usuario: ");
+    let contrasenaUsuario = prompt("Ingresa un PIN: ");
+    
+    if(isNaN(nuevoUsuario) && !isNaN(contrasenaUsuario)) {
+        console.log("Usuario no contiene valores numericos."+"\n\n"+"La contraseña es un valor numerico, no contiene letras."); 
+        usuario = nuevoUsuario;
+        contrasena = contrasenaUsuario;
+        idEmpleado = +idEmpleado + 1;
+        rol = '';
+        console.log("Usuario: " + usuario +  "\n" + "Contraseña: " + contrasena);
+        console.log("Usuario registrado exitosamente");   
+        empleado = new Empleado(usuario, contrasena, idEmpleado, rol)     
+    } else if (!isNaN(nuevoUsuario)){
+        alert("Error al registrar usuario, usuario no debe contener valores numericos");
+        console.log("Error al registrar usuario, usuario no debe contener valores numericos");
+    } else if (isNaN(contrasenaUsuario)){
+        alert("Error al registrar usuario, PIN debe ser un valor numerico.");
+        console.log("Error al registrar usuario, PIN debe ser un valor numerico.");
+    }
+    console.log(empleado)  
+}
+
+
+
+
+/* Funcion de inicio de sesion */
+
+function logIn() {
+
+    for (let i = 3; i > 0; i--){
+        let campoUsuario = prompt("Ingresa nombre de usuario: ");
+        let campoContrasena = prompt("Ingresa contraseña: ");
+
+            console.log("El usuario deberia ser: " + empleado.usuario);    
+            console.log("Usuario ingresado: " + campoUsuario);
+            console.log("La contraseña deberia ser: " + empleado.contrasena);    
+            console.log("Contraseña ingresada: " + campoContrasena);
+
+    if (campoUsuario === empleado.usuario && campoContrasena === empleado.contrasena){
+        alert("Ingresaste correctamente.")
+        window.location.href = "platform.html"   
+        console.log("Ingresaste correctamente") 
+    } else if (i>1){
+        alert("Usuario no encontrado."+"\n"+"Te quedan " + (i-1) + " intentos.")
+        console.log("Usuario no encontrado");
+    } else {
+        alert("Usuario no encontrado."+"\n"+"Superaste el limite de intentos, intenta mas tarde.")
+    }    
+    }
+
+    
+}
+
+
+
+
+/* for (let i = 3; i < 1; i--){ */
