f7683699fcefc95d9c4eb40727075985a02fe5b4
Arturo <Arturojportillo@gmail.com>
2024-08-06 05:26:06 -0300

    test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-if (window.location.pathname === '/index.html'){
+if (window.location.pathname == '/index.html'){
     alert("Bienvenido a MiBar! Sistema de gestion gastronomica" + "\n\n" +"Para iniciar sesion por primera vez debes registrarte.")
 } else {
     alert("Bienvenido a MiBar! Sistema de gestion gastronomica" + "\n" + "El sistema se encuentra en mantenimiento, vuelve mas tarde." )
