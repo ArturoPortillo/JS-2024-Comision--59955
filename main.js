219167eba37a1882c5b56f35e0989423a4db3cea
Arturo <Arturojportillo@gmail.com>
2024-08-31 14:27:39 -0300

    Tests

--- main.js
+++ main.js
@@ -1,55 +1,122 @@
-
 /* Solamente un pequeño script para evitar que el mensaje "Para iniciar sesion por primera vez debes registrarte." aparezca sin importar si estamos en la pagina de inicio o dentro de la plataforma, con esto logro que aparezca solamente en index.html */
 
 const paginaActual = window.location.href;
 
-if (paginaActual.includes('/index.html')) {
-    alert("Bienvenido a MiBar! Sistema de gestion gastronomica" + "\n\n" + "Para iniciar sesion por primera vez debes registrarte.");
-    console.log(`Bienvenido a MiBar Sistema de gestion gastronomica! Crea un nuevo usuario o inicia sesion como administrador:
+/* if (paginaActual.includes("/index.html")) {
+	alert(
+		"Bienvenido a MiBar! Sistema de gestion gastronomica" +
+			"\n\n" +
+			"Para iniciar sesion por primera vez debes registrarte."
+	);
+	console.log(`Bienvenido a MiBar Sistema de gestion gastronomica! Crea un nuevo usuario o inicia sesion como administrador:
         
         usuario: admin
         clave: admin
-        `)
+        `);
 } else {
-    alert("Bienvenido a MiBar! Sistema de gestion gastronomica" + "\n\n" + "El sistema se encuentra en mantenimiento, vuelve mas tarde.");
-}
+	alert(
+		"Bienvenido a MiBar! Sistema de gestion gastronomica" +
+			"\n\n" +
+			"El sistema se encuentra en mantenimiento, vuelve mas tarde."
+	);
+} */
 
 /* Variable usuario y contraseña */
 
 let usuario = "";
 let contrasena = "";
 let idEmpleado = 0;
-let rol = '';
+let rol = "";
 
 /* Funcion constructora de usuario estableciendo su rol */
 
 function Empleado(usuario, contrasena, idEmpleado, rol) {
-    this.usuario = usuario;
-    this.contrasena = contrasena;
-    this.idEmpleado = idEmpleado;
-    this.rol = rol;
+	this.usuario = usuario;
+	this.contrasena = contrasena;
+	this.idEmpleado = idEmpleado;
+	this.rol = rol;
 }
 
 /* Base de datos de usuarios registrados */
 
-const usuariosRegistrados = [{usuario: "admin", contrasena: "admin", idEmpleado: 0, rol: "admin"}]
+const usuariosRegistrados = [
+	{ usuario: "admin", contrasena: "admin", idEmpleado: 0, rol: "admin" },
+];
+
+/* Funcion popUp */
+
+let btnCerrar = document.querySelector('.cancelarRegistro')
+
+function popUp() {
+	
+	let btnCerrar = document.querySelector('.cancelarRegistro')
+	let popUp = document.querySelector('.popUpfunc')
+	/* Se generaba un bug que nos hacia "draggear" un div dentro de nuestro Input por error al tratar de sombrear el texto, lo solucione con el atributo: ondragstart="return false;" ondrop="return false;"
+	Sin embargo, no se puede sombrear con el Mouse dentro del Input, solo se puede sombrear con el atajo Shift + flecha Izquierda
+	Fuente: https://stackoverflow.com/questions/704564/disable-drag-and-drop-on-html-elements */
+	popUp.innerHTML = `
+		<div class="popUpform animate__animated animate__zoomIn" draggable="true" ondragstart="drag(event)" id="mydiv">
+			<div style="display: flex; flex-direction: row;">
+			<div class="title-bar" id="mydivheader">            
+				<div class="title-bar-text innerHeader" style="margin:0 12rem 0 0;padding:0;"> <img src="favicon2.png" alt="" class="innerFavicon">MiBar - Sistema de gestión Gastronomica</div>
+							<div class="title-bar-controls">
+				<button type="button" aria-label="Close" class="cancelarRegistro" style="height:16px;width;14px;margin-top:3px;" onclick="toggleVis()"></button>
+			</div> 
+			</div>
+
+			</div>
+		<fieldset class="popUpset" >
+			<div class="popUp">
+				<img src="LOGO MIBAR.png" alt="" class="logoMibarpop">
+				<div class="popForm">
+					<div class="field-row-stacked">
+					<label>Nuevo usuario:</label>
+					<input type="text" placeholder="Nuevo usuario" class="newUserfield"  ondragstart="return false;" ondrop="return false;"/>
+					</div>
+					<div class="field-row-stacked">
+					<label>Ingrese contraseña:</label>
+					<input type="password" placeholder="Ingrese contraseña" class="newPass"  ondragstart="return false;" ondrop="return false;"/>
+					</div>
+					<div>
+					<button type="button" onclick="registroUsuario()">Registrarse</button>
+					<button type="button" class="cancelarRegistro" onclick="toggleVis()">Cancelar</button>
+					</div>                 
+			</div>
+		</fieldset>
+		</div>
+		`
+	/* Fuente de funcion: https://www.w3schools.com/HOWTO/howto_js_draggable.asp */
+	dragElement(document.getElementById("mydiv"));
+	btnCerrar.addEventListener("click", toggleVis)
+	toggleVis()
+}
 
 
-/* Funcion de registro */
 
-const empleado = {
-    usuario: "",
-    contrasena: "",
-    idEmpleado: 0,
-    rol: "",
-};  
+
+function toggleVis() {
+
+	
+	let popUp = document.querySelector('.popUpfunc')
+    if (popUp.classList.contains('vis')) {
+        popUp.classList.remove('vis');
+        popUp.classList.add('invis');
+    } else {
+        popUp.classList.remove('invis');
+        popUp.classList.add('vis');
+    }
+	};
+
+btnCerrar.addEventListener("click", toggleVis)
+
+
+
 
 
 /* Funcion registrar usuario */
 
 function registroUsuario() {
-
-    /*  COMENTARIO SOBRE VALIDACIONES:  
+	/*  COMENTARIO SOBRE VALIDACIONES:  
     
     ########## NOTAS PARA EL FUTURO LECTOR #################
 
@@ -71,281 +138,84 @@ function registroUsuario() {
         Dejando nuestra RegEx asi (/\s+/g) luego usando el metodo de string .replace declaramos que reemplazamos todos los espacios en blanco por un espacio vacio: (/\s+/g, '') como en el ejemplo anterior los reemplazabamos por un # ahora lo reemplazamos por un espacio vacio << '' >>, recortando nuestro texto totalmente, evitando que el usuario agregue espacios en blanco al campo de usuario o contraseña.
     */
 
-    let nuevoUsuario = prompt("Ingresa nuevo nombre de usuario: ").toLowerCase().replace(/\s+/g, '');
-    let contrasenaUsuario = prompt("Ingresa un PIN: ").toLowerCase().replace(/\s+/g, '');
-    
-    if(isNaN(nuevoUsuario) && !isNaN(contrasenaUsuario)) {
-        console.log("Usuario no contiene valores numericos."+"\n\n"+"La contraseña es un valor numerico, no contiene letras."); 
 
-        usuario = nuevoUsuario;
-        contrasena = contrasenaUsuario;
-        idEmpleado = +idEmpleado + 1;
-        rol = '';
+	let inputNuevousuario = document.querySelector(".newUserfield");
+	console.log(inputNuevousuario.value)
+	let nuevoUsuario = inputNuevousuario.value.toLowerCase().replace(/\s+/g, "");
 
-        console.log("Usuario: " + usuario +  "\n" + "Contraseña: " + contrasena);
-        console.log("Usuario registrado exitosamente");   
+	let inputNuevapass = document.querySelector(".newPass");
+	console.log(inputNuevapass.value);
+	
+	let contrasenaUsuario = inputNuevapass.value.toLowerCase().replace(/\s+/g, "");
 
-        empleado.usuario = usuario;
-        empleado.contrasena = contrasena;
-        empleado.idEmpleado = idEmpleado;
-        empleado.rol = rol;
+	if (isNaN(nuevoUsuario) && !isNaN(contrasenaUsuario)) {
+		console.log(
+			"Usuario no contiene valores numericos." +
+				"\n\n" +
+				"La contraseña es un valor numerico, no contiene letras."
+		);
 
-        const nuevoEmpleado = new Empleado(usuario, contrasena, idEmpleado, rol);
-        usuariosRegistrados.push(nuevoEmpleado)
-        console.log(nuevoEmpleado)
-        console.table(usuariosRegistrados)
+		usuario = nuevoUsuario;
+		contrasena = contrasenaUsuario;
+		idEmpleado = +idEmpleado + 1;
+		rol = "";
 
-    } else if (!isNaN(nuevoUsuario)){
-        alert("Error al registrar usuario, usuario no debe contener valores numericos");
-        console.log("Error al registrar usuario, usuario no debe contener valores numericos");
-    } else if (isNaN(contrasenaUsuario)){
-        alert("Error al registrar usuario, PIN debe ser un valor numerico.");
-        console.log("Error al registrar usuario, PIN debe ser un valor numerico.");
-    }
+		console.log("Usuario: " + usuario + "\n" + "Contraseña: " + contrasena);
+		console.log("Usuario registrado exitosamente");
+
+		const nuevoEmpleado = new Empleado(usuario, contrasena, idEmpleado, rol);
+		usuariosRegistrados.push(nuevoEmpleado);
+		console.log(nuevoEmpleado);
+		console.table(usuariosRegistrados);
+	} else if (!isNaN(inputNuevousuario)) {
+		alert(
+			"Error al registrar usuario, usuario no debe contener valores numericos"
+		);
+		console.log(
+			"Error al registrar usuario, usuario no debe contener valores numericos"
+		);
+	} else if (isNaN(inputNuevapass)) {
+		alert("Error al registrar usuario, PIN debe ser un valor numerico.");
+		console.log("Error al registrar usuario, PIN debe ser un valor numerico.");
+	}
 }
+
 
 /* Funcion inicio de sesion */
 
-    function inicioSesion() {
+function inicioSesion() {
+	const btnInicio = document.querySelector(".userField");
+	console.log(btnInicio);
 
-        let validarDatos = false;
-        let usuarioActivo;
+	let validarDatos = false;
+	let usuarioActivo;
 
-        console.log("Usuarios registrados: ")
-        usuariosRegistrados.map( usuariosRegistrado => console.log(usuariosRegistrado))
-        
-        for (let i = 0; i < 3; i++){
+	console.log("Usuarios registrados: ");
+	usuariosRegistrados.map((usuariosRegistrado) =>
+		console.log(usuariosRegistrado)
+	);
 
-            let campoUsuario = prompt("Ingresa nombre de usuario: ").toLowerCase().replace(/\s+/g, '');
-            let campoContrasena = prompt("Ingresa contraseña: ").toLowerCase().replace(/\s+/g, ''); 
+	const campoUsuario = document.querySelector(".userField");
+	let inputUsuario = campoUsuario.value.toLowerCase().replace(/\s+/g, "");
+	console.log(inputUsuario);
 
+	const campoContrasena = document.querySelector(".inputPass");
+	let inputPass = campoContrasena.value.toLowerCase().replace(/\s+/g, "");
 
-            usuariosRegistrados.forEach(usuario => {
-
-                if (usuario.usuario == campoUsuario && usuario.contrasena == campoContrasena) {
-                    alert("Ingresaste correctamente")
-                    usuarioActivo = campoUsuario;
-                    console.log("el usuario activo es: "+ usuarioActivo)
-                    window.location.href = "platform.html";             
-                    validarDatos = true;
-                }
-            })
-
-            if (validarDatos) { 
-                break;
-            } else if (i<2) {
-                alert("Usuario no encontrado o contraseña incorrecta. Te quedan " + (2 - i) + " intentos.");
-            } else {
-                alert("Usuario no encontrado o contraseña incorrecta. Superaste el límite de intentos, intenta más tarde.");
-            }
-            
-        }         
-        return usuarioActivo;
-    }
-
-/* Funcion constructora del menu*/
-
-function Producto (id, articulo, precio) {
-    this.id= id;
-    this.articulo = articulo;
-    this.precio = precio;
+	usuariosRegistrados.forEach((usuario) => {
+		if (usuario.usuario == inputUsuario && usuario.contrasena == inputPass) {
+			alert("Ingresaste correctamente");
+			usuarioActivo = inputUsuario;
+			localStorage.setItem("usuarioActivo", inputUsuario);
+			console.log("el usuario activo es: " + usuarioActivo);
+			window.location.href = "platform.html";
+			validarDatos = true;
+		}
+	});
 }
 
-const datosMenu = [{articulo: "Café con leche", precio: 3100, id: 1}
-];
-
-let articulo;
-let precio = 0;
-let id = 1;
-
-
-
-/* Funcion para ver el menu disponible */
-
-function menuDisponible() {
-    console.log(`Articulos disponibles: `);            
-    datosMenu.forEach((menu)=>{
-        const contenido=`ID: ${menu.id}\nArticulo: ${menu.articulo}\nPrecio: $${menu.precio}`
-        console.log(contenido)
-    });
-    return datosMenu;
-}
-/* FUNCION CON .MAP QUE REGRESABA EL ARRAY COMO STRING Y NO ME SERVIA PARA LA FUNCION abrirMesa() */
-/* function menuDisponible() {
-
-    console.log(`Articulos disponibles: `);            
-    const menu = datosMenu.map((menu)=>{
-            const contenido=`ID: ${menu.id}\nArticulo: ${menu.articulo}\nPrecio: $${menu.precio}`
-            console.log(contenido)
-                return contenido;
-            });
-    return menu;
-} */
-
-
-/* Funcion para cargar Articulos al array de menu */
-
-function cargarMenu() {
-
-    while (true){
-        let nuevoArticulo = prompt("Ingresa un nuevo articulo: " + "\n\n" + "Ingresa '0' para salir.").trim();
-        
-        if (nuevoArticulo === "0") {
-            console.log("No se han cargado más articulos.")
-            break;
-        } 
-        
-        let nuevoPrecio = parseInt(prompt("ingresa precio del articulo: " + "\n\n" + "Ingresa '0' para salir."));
-        
-        if (nuevoArticulo === "0") {
-            console.log("No se han cargado más articulos.")
-            break;
-        } 
-                
-        if (!isNaN(nuevoArticulo) || isNaN(nuevoPrecio)) {
-            console.log("Ingresa datos validos")
-            break;
-        } else {
-                    
-        id += 1;
-        const producto = {
-            articulo: nuevoArticulo,
-            precio: nuevoPrecio,
-            id: id
-        };
-
-        datosMenu.push(producto);
-        console.table(datosMenu);
-        menuDisponible()
-        }
-        }
-    };
-
-/* Array de mesas */
-
-    const arrMesas = [[],[],[]]
-    
-
-function crearMesa() {
-    let nuevaMesa = [];
-    let arrLength = arrMesas.length + 1;
-    arrMesas.push(nuevaMesa)
-    console.log("creaste una nueva mesa.")
-    console.log("Numero de mesa creada: " + arrLength)
-    console.log(arrMesas)
-}
-
-function recorrerMesas() {
-    console.log(`Las mesas disponibles son: `)  
-    arrMesas.forEach((arr)=>{
-        console.log(arr)
-    });
-    return arrMesas
-}
-
-/* Funcion para abrir una mesa y agregar articulos a la mesa seleccionada */
-
-function abrirMesa() {
-
-    let mesaSelec = parseInt(prompt("Indica el numero de mesa que deseas abrir: "))
-
-        if (isNaN(mesaSelec) || mesaSelec < 1 || mesaSelec > arrMesas.length) {
-            console.log(`Ingresa un numero valido de mesa`)
-            return;
-        }
-    let eleccion = mesaSelec - 1;
-
-    console.log("Ingresaste a la mesa "+mesaSelec)
-    
-    console.log("Ingresa el ID del articulo que deseas agregar:");
-    const menu = menuDisponible();
-    let idArticulo = parseInt(prompt("Ingresa el ID del articulo que deseas agregar:"));
-    
-    if (isNaN(idArticulo)) {
-        console.log("El ID ingresado no es un número.");
-        return;
-    }
-
-    const artIngresado = menu.find((producto) => producto.id === idArticulo);
-    
-    if (artIngresado) {
-        console.log(`El ID ingresado es ${idArticulo}. Artículo: ${artIngresado.articulo}, Precio: $${artIngresado.precio}`);
-        arrMesas[eleccion].push(artIngresado)
-    } else {
-        console.log("No se encontró ningún artículo con ese ID.");
-    }
-    console.table(arrMesas)
-    }
-
-/* FUNCIONES DE PESTAÑAS */
-
-    document.addEventListener("DOMContentLoaded", function() {
-
-        const tabs = document.querySelectorAll('li[role="tab"]');
-    
-        tabs.forEach(tab => {
-            tab.addEventListener('click', function() {
-                tabs.forEach(t => t.setAttribute('aria-selected', 'false'));
-                
-            tab.setAttribute('aria-selected', 'true');
-            });
-        });
-    });
-
-    /* TEST */
-
-    const date=new Date()
-    
-    if (paginaActual.includes('platform.html')) {
-        document.querySelector('.fecha').textContent= `Fecha: ${date.toLocaleDateString()} | Hora: ${date.toLocaleTimeString()};`;
-
-        alert("Abre la consola para acceder a las opciones disponibles")
-
-        console.log(`
-            
-            Funciones disponibles dentro de la plataforma: 
-
-            1. menuDisponible(): nos permite ver el listado de articulos disponibles.
-
-            2. cargarMenu(): nos permite cargar articulos a nuestro menú.
-
-            3. crearMesa(): nos permite agregar una mesa vacia a nuestro array de mesas.
-
-            4. abrirMesa(): nos permite abrir una mesa y cargar articulos a una mesa.
-            `)
-    }
-
-/*     document.querySelector('.usuarioActivo').textContent=`Usuario: ${usuarioActivo};`; */
-
-
-/* Codigo viejo
-
-Funcion de inicio de sesion 
-
-PRIMER FUNCION DE INICIO DE SESION, REEMPLAZADA POR UNA QUE BUSQUE EL USUARIO DENTRO DE NUESTRO ARRAY DE USUARIOS.
-    function inicioSesion() {
-
-    for (let i = 0; i < 3; i++){
-        let campoUsuario = prompt("Ingresa nombre de usuario: ").toLowerCase().replace(/\s+/g, '');
-        let campoContrasena = prompt("Ingresa contraseña: ").toLowerCase().replace(/\s+/g, '');
-
-            console.log("El usuario deberia ser: " + empleado.usuario);    
-            console.log("Usuario ingresado: " + campoUsuario);
-            console.log("La contraseña deberia ser: " + empleado.contrasena);    
-            console.log("Contraseña ingresada: " + campoContrasena);
-
-    if (campoUsuario === empleado.usuario && campoContrasena === empleado.contrasena){
-        alert("Ingresaste correctamente.");
-        window.location.href = "platform.html"; 
-        console.log("Ingresaste correctamente");
-        break;
-    } else if (i<2){
-        alert("Usuario no encontrado."+"\n"+"Te quedan " + (2 - i) + " intentos.");
-        console.log("Usuario no encontrado");
-    } else {
-        alert("Usuario no encontrado."+"\n"+"Superaste el limite de intentos, intenta mas tarde.")
-        }    
-    }
-} */
+const btnInicio = document.querySelector(".iniciarSesion");
+btnInicio.addEventListener("click", inicioSesion);
+console.log(btnInicio);
 
 
---
+++ plano.js
@@ -0,0 +1,347 @@
+/* Funcion para recordar usuario en sesion */
+
+function usuarioIniciado() {
+	const nombreUsuario = localStorage.getItem("usuarioActivo");
+	console.log(nombreUsuario);
+	document.querySelector(
+		".usuarioActivo"
+	).textContent = `Usuario: ${nombreUsuario}`;
+	return nombreUsuario;
+}
+
+/* Funcion constructora del menu*/
+
+function Producto(id, articulo, precio) {
+	this.id = id;
+	this.articulo = articulo;
+	this.precio = precio;
+}
+
+const datosMenu = [
+	{ articulo: "Café con leche", precio: 3100, id: 1 },
+	{ articulo: "Tostado de miga", precio: 5200, id: 2 },
+	{ articulo: "Licuado", precio: 3600, id: 3 },
+];
+
+let articulo;
+let precio = 0;
+let id = datosMenu.length;
+
+/* Funcion para ver el menu disponible */
+
+let artMenu = document.querySelector(".panelMenu");
+
+let btnMenu = document.querySelector(".addMenu");
+console.log(btnMenu);
+
+let contenidoLog;
+
+function menuDisponible() {
+	console.log(`Articulos disponibles: `);
+	artMenu.innerHTML = "";
+	datosMenu.forEach((menu) => {
+		contenidoLog = `ID: ${menu.id}\nArticulo: ${menu.articulo}\nPrecio: $${menu.precio}`;
+		let contenido = `<button class="btnaddArt">${menu.articulo}</button>`;
+		console.log(contenidoLog);
+		artMenu.innerHTML += contenido;
+	});
+	return datosMenu;
+}
+menuDisponible();
+
+/* Funcion para cargar Articulos al array de menu */
+
+function cargarMenu() {
+	while (true) {
+		let nuevoArticulo = prompt(
+			"Ingresa un nuevo articulo: " + "\n\n" + "Ingresa '0' para salir."
+		).trim();
+
+		if (nuevoArticulo === "0") {
+			console.log("No se han cargado más articulos.");
+			break;
+		}
+
+		let nuevoPrecio = parseInt(
+			prompt(
+				"ingresa precio del articulo: " + "\n\n" + "Ingresa '0' para salir."
+			)
+		);
+
+		if (nuevoArticulo === "0") {
+			console.log("No se han cargado más articulos.");
+			break;
+		}
+
+		if (!isNaN(nuevoArticulo) || isNaN(nuevoPrecio)) {
+			console.log("Ingresa datos validos");
+			break;
+		} else {
+			id += 1;
+			const producto = {
+				articulo: nuevoArticulo,
+				precio: nuevoPrecio,
+				id: id,
+			};
+
+			datosMenu.forEach((menu) => {
+				contenidoLog = `ID: ${menu.id}\nArticulo: ${menu.articulo}\nPrecio: $${menu.precio}`;
+				let contenido = `<button class="btnaddArt">${menu.articulo}</button>`;
+				console.log(contenidoLog);
+				artMenu.innerHTML += contenido;
+			});
+
+			datosMenu.push(producto);
+			console.table(datosMenu);
+			menuDisponible();
+		}
+	}
+}
+
+btnMenu.addEventListener("click", cargarMenu);
+
+/* Array de mesas */
+
+const arrMesas = [];
+
+function crearMesa() {
+	let nuevaMesa = [];
+
+	let arrLength = arrMesas.length + 1;
+	arrMesas.push(nuevaMesa);
+	console.log("creaste una nueva mesa.");
+	console.log("Numero de mesa creada: " + arrLength);
+	console.log(arrMesas);
+
+	/* console.log("Estamos buscando apuntar a: ") */
+	let target = document.querySelector("#plano");
+	/* console.log(target) */
+
+	let templateMesa = `<div class="modeloMesa" id="${arrLength}" onclick="abrirMesa(this)" draggable="true" ondragstart="drag(event)">MESA ${arrLength}</div>`;
+
+	let mesaExistente = target.querySelector(`.grilla${arrLength}`);
+
+	/* condicional con Operarador ternario 
+
+    mesaExistente = true
+		? (mesaExistente.innerHTML += templateMesa)
+		: alert("No se pueden crear más mesas, has llegado al limite del plano.");
+    */
+
+	if (mesaExistente) {
+		/* Si no pongo += sobreescrimos la mesa y borramos contenidos sin querer, esto nos permite "solapar" mesas si movimos una mesa a una posicion adelantada y creamos mesas en divs anteriores a la ultima.*/
+		mesaExistente.innerHTML += templateMesa;
+	} else {
+		console.log(
+			`No se pueden crear más mesas, has llegado al limite del plano.`
+		);
+		alert("No se pueden crear más mesas, has llegado al limite del plano.");
+	}
+	return arrLength;
+}
+
+/* Funcion a borrar. */
+function recorrerMesas() {
+	console.log(`Las mesas disponibles son: `);
+	arrMesas.forEach((arr) => {
+		console.log(arr);
+	});
+	return arrMesas;
+}
+
+/* Funcion para abrir una mesa y agregar articulos a la mesa seleccionada */
+
+function abrirMesa(mesa) {
+	/*     panelMesa.style.display = "block"; */
+	console.log(mesa);
+
+	/*  let arrLength = arrMesas.length;    
+    console.log("Ingresaste a mesa " + (arrLength - 1));
+
+    let mesaSelec = parseInt(prompt("Indica el numero de mesa que deseas abrir: "));
+ */
+	let mesaSelec = parseInt(mesa.id);
+
+	if (isNaN(mesaSelec) || mesaSelec < 1 || mesaSelec > arrMesas.length) {
+		console.log(`Ingresa un numero valido de mesa`);
+		return;
+	}
+
+	let eleccion = mesaSelec - 1;
+	/*     console.log('El valor de eleccion es ' + eleccion + 1); */
+
+	console.log("Ingresaste a la mesa " + (eleccion + 1));
+
+	console.log("Ingresa el ID del articulo que deseas agregar:");
+	const menu = menuDisponible();
+	let idArticulo = parseInt(
+		prompt(
+			"Ingresa el ID del articulo que deseas agregar:\nIngresa '0' para salir"
+		)
+	);
+
+	if (isNaN(idArticulo)) {
+		console.log("El ID ingresado no es un número.");
+		return;
+	}
+
+	const artIngresado = menu.find((producto) => producto.id === idArticulo);
+
+	if (artIngresado) {
+		console.log(
+			`El ID ingresado es ${idArticulo}. Artículo: ${artIngresado.articulo}, Precio: $${artIngresado.precio}`
+		);
+		arrMesas[eleccion].push(artIngresado);
+		console.log(`El contenido de la mesa es:
+            `);
+		console.log(arrMesas[eleccion]);
+	}
+	if ((idArticulo = 0 || "0")) {
+		console.log("No agregaste ningun articulo.");
+	} else {
+		console.log("No se encontró ningún artículo con ese ID.");
+		return;
+	}
+	console.log("Contenido de todas las mesas:");
+
+	console.table(arrMesas);
+
+	let contenidoArr = document.querySelector("tbody");
+
+	let getData = arrMesas[eleccion];
+
+	contenidoArr.innerHTML = "";
+
+	let contenidoMesa = "";
+
+	console.log(getData);
+	getData.forEach((obj) => {
+		contenidoMesa += `
+                    <tr>
+                        <td>${obj.articulo}</td>
+                        <td>cantidad</td>
+                        <td>${obj.precio}</td>                  
+                        <td>${obj.precio}</td>     
+                    </tr>
+        `;
+		console.log(obj);
+	});
+	contenidoArr.innerHTML = contenidoMesa;
+
+	return eleccion;
+}
+/* Funcion para bloquear y desbloquear el plano */
+
+console.log("Buscamos el candado en: ");
+
+let bloquearPlano = document.querySelector(".lock");
+
+console.log(bloquearPlano);
+
+function cambiarCandado() {
+	bloquearPlano.addEventListener("click", () => {
+		console.log("Estado del candado");
+
+		let estadoCandado = bloquearPlano.src;
+		console.log("src del candado: ");
+		console.log(estadoCandado);
+
+		/*Selector de classes que "empiezan con" https://www.w3schools.com/cssref/sel_attr_begin.php */
+		let mesasVisibles = document.querySelectorAll('[class^="grilla"]');
+		let sumarMesa = document.querySelector(".addTable");
+		console.log(sumarMesa);
+		console.log(mesasVisibles);
+
+		if (estadoCandado.includes("Close")) {
+			console.log("Esta cerrado");
+			candadoPlano = document.querySelector(".lock").src = "/Open.png";
+			sumarMesa.style.visibility = "visible";
+			mesasVisibles.forEach((mesa) => {
+				mesa.style.visibility = "visible";
+			});
+			console.log("asi que lo abrimos.");
+		} else if (estadoCandado.includes("Open")) {
+			console.log("Esta unlocked");
+			candadoPlano = document.querySelector(".lock").src = "/Close.png";
+			sumarMesa.style.visibility = "hidden";
+			mesasVisibles.forEach((mesa) => {
+				mesa.style.visibility = "hidden";
+			});
+			console.log("Asi que lo cerramos");
+		}
+	});
+}
+
+/* Funciones de Drag
+Mesas: Fuente: https://www.w3schools.com/html/tryit.asp?filename=tryhtml5_draganddrop2 */
+/* Pop up: https://www.w3schools.com/HOWTO/tryit.asp?filename=tryhow_js_draggable */
+
+/* DRAG MESAS  */
+function allowDrop(ev) {
+	ev.preventDefault();
+}
+
+function drag(ev) {
+	ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	ev.target.appendChild(document.getElementById(data));
+}
+
+/* FUNCIONES DE PESTAÑAS */
+
+document.addEventListener("DOMContentLoaded", function () {
+	const tabs = document.querySelectorAll('li[role="tab"]');
+
+	tabs.forEach((tab) => {
+		tab.addEventListener("click", function () {
+			tabs.forEach((t) => t.setAttribute("aria-selected", "false"));
+
+			tab.setAttribute("aria-selected", "true");
+		});
+	});
+});
+
+document.querySelectorAll("table.interactive").forEach((element) => {
+	element.addEventListener("click", (event) => {
+		const row = event.path.find(
+			(element) =>
+				element.tagName === "TR" && element.parentElement.tagName === "TBODY"
+		);
+		if (row) {
+			row.classList.toggle("highlighted");
+		}
+	});
+});
+
+/* Crear estructura en la pestaña a traves de DOM */
+
+/* TEST */
+
+const date = new Date();
+
+if (paginaActual.includes("platform.html")) {
+	usuarioIniciado();
+	document.querySelector(
+		".fecha"
+	).textContent = `Fecha: ${date.toLocaleDateString()} | Hora: ${date.toLocaleTimeString()};`;
+	usuarioIniciado();
+
+	alert("Abre la consola para acceder a las opciones disponibles");
+
+	console.log(`
+            
+            Funciones disponibles dentro de la plataforma: 
+
+            1. menuDisponible(): nos permite ver el listado de articulos disponibles.
+
+            2. cargarMenu(): nos permite cargar articulos a nuestro menú.
+
+            3. crearMesa(): nos permite agregar una mesa vacia a nuestro array de mesas.
+
+            4. abrirMesa(): nos permite abrir una mesa y cargar articulos a una mesa.
+            `);
+}
